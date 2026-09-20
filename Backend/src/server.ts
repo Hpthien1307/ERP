63df7beb5874fb001752579d5dc980e5677577e4
Leo Do <leo.do@thomasnet.com>
2026-09-20 16:29:58 +0700

    update cors in sever

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 8080
 app.use(helmet())
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL as string, "http://localhost:5173"],
     credentials: true
   })
 )
